432ad3d5ee4a3817422e301e00cc228a8520a6ea
zeyad-ahmed-samer <zeyadbasha1@hotmail.com>
2022-06-07 14:15:58 +0200

    first commit

---
+++ B87SApp.py
@@ -0,0 +1,137 @@
+
+
+
+asciFile=open("input.txt","r")
+originalData=asciFile.read()
+
+print("original data:",originalData)
+
+datalength=len(originalData)
+
+output=""
+
+lastPolarity="-"
+
+flag=False
+onesCounter=0
+
+def checkEightZeroes(index):
+      for j in range(1,8):
+                    if(originalData[index+j]=='0'):
+                         flag=True
+                    else:
+                         flag=False
+                         break
+      return flag               
+
+
+i=0
+
+while(i<datalength):
+
+
+     #Case 1
+     if(originalData[i]=='1'):
+          if(lastPolarity=='-'):
+              
+               output+='+'
+               lastPolarity='+'
+
+          else:# if lastpolarity +ve
+                 
+                 output+='-'
+                 lastPolarity='-'
+
+
+
+   #Case 0
+     else:
+          if(datalength-i>=8):
+               #finding 8 zeroes
+               flag=checkEightZeroes(i)
+              
+          else:
+               output+='0' 
+               i+=1
+               continue            ## should continue
+
+          # if we found * zeroes flag will be equal true,then we will do the following
+          if(flag==True):
+               #000
+               for j in range(0,3):
+                  
+                    output+='0'
+
+
+               #V
+               if(lastPolarity=='+'):
+                   output+='+'
+                   onesCounter+=1
+                   lastPolarity='+'
+                 
+               else:
+                  output+='-'
+                  onesCounter+=1
+                  lastPolarity='-'
+                
+
+               #B
+               if(lastPolarity=='-'):
+                   output+='+'
+                   onesCounter+=1
+                   lastPolarity='+'
+                
+               else:
+                    output+='-'
+                    onesCounter+=1
+                    lastPolarity='-'
+                   
+
+
+               #0
+
+               output+='0'
+
+               #V
+               if(lastPolarity=='+'):
+                   output+='+'
+                   onesCounter+=1
+                   lastPolarity='+'
+               else:
+                  output+='-'
+                  onesCounter+=1
+                  lastPolarity='-'
+
+
+               #B again
+               if(lastPolarity=='-'):
+                   output+='+'
+                   onesCounter+=1
+                   lastPolarity='+'
+               else:
+                    output+='-'
+                    onesCounter+=1
+                    lastPolarity='-'
+          
+               i+=7
+          # case 8 places were found but no 8 zeroes(flag=flase)  
+          else:
+               output+='0'        
+
+
+     i=i+1
+     flag=False
+    
+
+
+
+print(output)
+
+
+OutputFile=open("B87SEncoded.txt","a")
+OutputFile.write(output)
+OutputFile.close()
+
+
+
+
---
+++ hdb3.py
@@ -0,0 +1,115 @@
+
+
+# note , i assumed that i have even number of ones at the beggining
+
+asciFile=open("input.txt","r")
+originalData=asciFile.read()
+
+print("Original Data:"+originalData)
+
+datalength=len(originalData)
+print(datalength)
+
+output="" 
+
+print("ou"+output)
+zeroCounter=0
+onesCounter=0
+i=0
+while(i<datalength):
+
+    print("i:",i)
+ 
+     #zero case
+    if(originalData[i]=='0'):
+        ## checking how much is left from the string , if less than 4 chars is left then 4 zeroes case willnot happen
+        #if(datalength - i >=4):
+             
+
+        #checking no 4 zeros occurs first 
+
+          if (datalength-i>=4 and originalData[i+1]=='0' and originalData[i+2]=='0' and originalData[i+3]=='0' ):
+
+
+            # checking the number of previos ones
+               if(onesCounter%2==0):
+            #applying bipolar
+                #checking perceding pulse bit
+
+                   #debug 
+                    
+                     if(output[i-1]=='-'):
+                       
+                       output=output+'+'+'0'+'0'+'+'
+                       i+=3
+                       onesCounter+=2
+                       
+                     elif(output[i-1]=='+'):
+                    
+                        output=output+'-'+'0'+'0'+'-'
+                        i+=3
+                        onesCounter+=2
+
+            # not even  number of ones
+               else:
+                      if(output[i-1]=='-'):
+                           
+                           output=output+'0'+'0'+'0'+'-'
+                           
+                           i+=3
+                           onesCounter+=1
+
+
+                
+                    
+                      elif(output[i-1]=='+'):
+                           output=output+'0'+'0'+'0'+'+'
+                           i+=3
+                           onesCounter+=1
+
+
+
+                   
+
+          else:
+                 output+='0'            
+                
+
+
+
+
+   ## 1 case
+    else:
+         
+
+         if(onesCounter%2==0):
+              output+='+'
+         else:
+              output+='-'    
+
+              
+         onesCounter+=1
+  
+  
+    i+=1
+    print(i)
+    print(onesCounter)
+     
+
+
+
+   
+print("encoded data:"+output)
+    
+outputFile=open("hdb3Output.txt",'a')
+outputFile.write(output)
+outputFile.close()
+
+
+
+      
+
+
+
+    
+
